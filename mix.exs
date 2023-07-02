fbe8e2093e95855bbafbffc730698f2f3df54948
Owen Wheary <owen356wh@gmail.com>
2023-07-01 17:05:51 -0700

    0.1.1: updated readme

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Chrysopoeia.MixProject do
   def project do
     [
       app: :chrysopoeia,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       description: description(),
